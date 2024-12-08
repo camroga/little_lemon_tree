77df04365b40c5a8bbce10f95c2a6fbc3bddd8fb
Camilo Andrés Rodríguez Garzón <cam_rod@hotmail.com>
2024-12-08 12:52:49 +0400

    apply modifiers for text and image

--- C5M5L1 Item 05 Starter Code/app/src/main/java/com/example/littlelemon/LowerPanel.kt
+++ C5M5L1 Item 05 Starter Code/app/src/main/java/com/example/littlelemon/LowerPanel.kt
@@ -4,13 +4,17 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -50,13 +54,33 @@ fun MenuDish(navController: NavHostController? = null, dish: Dish) {
         Log.d("AAA", "Click ${dish.id}")
         navController?.navigate(DishDetails.route + "/${dish.id}")
     }) {
-        Row {
-            Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column(
+//                modifier = Modifier
+//                    .fillMaxWidth(0.75f)
+            ) {
                 Text(dish.name, style = MaterialTheme.typography.h2)
-                Text(dish.description, style = MaterialTheme.typography.body1)
+                Text(
+                    dish.description,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier
+                        .fillMaxWidth(0.75f)
+                        .padding(top = 5.dp, bottom = 5.dp)
+                )
                 Text("${dish.price}", style = MaterialTheme.typography.body2)
             }
-            Image(painter = painterResource(id = dish.imageResource), contentDescription = "image")
+            Image(
+                painter = painterResource(id = dish.imageResource),
+                contentDescription = "image",
+                modifier = Modifier
+                    .clip(RoundedCornerShape(10.dp))
+                    .fillMaxSize(),
+//                contentScale = ContentScale.Crop
+            )
 
         }
     }
